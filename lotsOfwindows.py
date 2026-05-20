8948354b16bae25062b833ac0828541c4ef2905f
omaybhambanipunjabi-cell <omaybhambanipunjabi@gmail.com>
2026-05-20 18:16:10 +0100

    Add files via upload

---
+++ lotsOfwindows.py
@@ -0,0 +1,23 @@
+from tkinter import *
+
+root = Tk()
+root.geometry("400x300")
+root.title("Main")
+
+def topwin():
+    top = Toplevel()
+    top.geometry("180x100")
+    top.title("TopLevel")
+
+    l2 = Label(top, text="This is a TopLevel window")
+    l2.pack()
+
+    top.mainloop()
+
+l = Label(root, text="This is not a root window")
+btn = Button(root, text="Click here to open another window", command=topwin)
+
+l.pack()
+btn.pack()
+
+root.mainloop()
